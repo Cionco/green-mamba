173ac573ebedd2a8b8f6e53c144cddc2f1b05425
Nicolas Kepper <nicolas.kepper@gmail.com>
2019-09-17 09:06:16 +0200

    Cant run into the wall anymore

--- project/scripts/Snake.gd
+++ project/scripts/Snake.gd
@@ -1,6 +1,6 @@
 extends Sprite
 
-var speed = 1
+var speed = 5
 var velocity = [0, 0]
 # Declare member variables here. Examples:
 # var a = 2
@@ -12,14 +12,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_key_pressed(KEY_LEFT):
+	if Input.is_key_pressed(KEY_LEFT) and velocity[0] == 0:
 		velocity = [-speed, 0]
-	elif Input.is_key_pressed(KEY_RIGHT):
+		self.rotation_degrees = -90
+	elif Input.is_key_pressed(KEY_RIGHT) and velocity[0] == 0:
 		velocity = [speed, 0]
-	elif Input.is_key_pressed(KEY_UP):
+		self.rotation_degrees = 90
+	elif Input.is_key_pressed(KEY_UP) and velocity[1] == 0:
 		velocity = [0, -speed]
-	elif Input.is_key_pressed(KEY_DOWN):
+		self.rotation_degrees = 0
+	elif Input.is_key_pressed(KEY_DOWN) and velocity[1] == 0:
 		velocity = [0, speed]
+		self.rotation_degrees = 180
 	
-	self.position.x += velocity[0]
-	self.position.y += velocity[1]
+	if self.position.x + velocity[0] > 0 and self.position.x + velocity[0] < get_viewport_rect().size.x:
+		self.position.x += velocity[0]
+	if self.position.y + velocity[1] > 0 and self.position.y + velocity[1] < get_viewport_rect().size.y:
+		self.position.y += velocity[1]
